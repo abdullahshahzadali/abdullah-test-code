9ae7b51ab14c643f2a10a59f6256e33e6078d633
Abdullah Shahzad <abdullahshahzadali2004@gmail.com>
2025-03-06 22:44:06 +0500

    initial-commit

---
+++ lab3(listing).py
@@ -0,0 +1,40 @@
+numbers_list = [1, 2, 3, 4, 5]
+
+# A list of strings
+fruits_list = ['apple', 'banana', 'cherry']
+
+# A list of floats
+float_list = [1.5, 2.7, 3.14, 4.8, 5.0]
+
+# A list of booleans
+boolean_list = [True, False, True, True, False]
+
+# A mixed list
+mixed_list = [1, 'apple', 3.14, True]
+fruits_list[1]='blueberry'
+print(fruits_list)
+fruits_list.append('orange')
+print(fruits_list)
+fruits_list.insert(1,'kiwi')
+print(fruits_list)
+last_fruit=fruits_list.pop()
+print(last_fruit)
+fruits_list.clear()
+print(fruits_list)
+number=[0,1,2,3,4,5,6]
+print(number[1:4])
+print(number[:3])
+print(number[4:])
+print(len(number))
+number.sort
+print(number)
+number.reverse()
+print(number)
+print(fruits_list.count('kiwi'))
+my_list=[]
+for i in range(5):
+    value = input("enter a number")
+    my_list.append(value)
+    print("the given value")
+    print(my_list)
+
